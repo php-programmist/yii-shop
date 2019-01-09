16b6ac94d230f76062484ead2beb3c43b4eb02a4
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-01-09 12:54:13 +0300

    lte IE9 files connected

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -10,6 +10,8 @@ use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
 AppAsset::register($this);
+$this->registerJsFile('js/html5shiv.js',['position' => yii\web\View::POS_HEAD,'condition' =>'lte IE9']);
+$this->registerJsFile('js/respond.min.js',['position' => yii\web\View::POS_HEAD,'condition' =>'lte IE9'])
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -20,11 +22,8 @@ AppAsset::register($this);
     
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+  
     
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
     <link rel="shortcut icon" href="images/ico/favicon.ico">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
