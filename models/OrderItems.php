b0ea67fc87a98e520180b91842833a4b37a70195
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-01-11 16:03:59 +0300

    order and orderItems models added

---
+++ models/OrderItems.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "order_items".
+ *
+ * @property int $id
+ * @property int $order_id
+ * @property int $product_id
+ * @property string $name
+ * @property double $price
+ * @property int $qty_item
+ * @property double $sum_item
+ *
+ * @property Order $order
+ * @property Product $product
+ */
+class OrderItems extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'order_items';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['order_id', 'product_id', 'qty_item'], 'integer'],
+            [['name', 'price', 'qty_item', 'sum_item'], 'required'],
+            [['price', 'sum_item'], 'number'],
+            [['name'], 'string', 'max' => 255],
+            [['order_id'], 'exist', 'skipOnError' => true, 'targetClass' => Order::class, 'targetAttribute' => ['order_id' => 'id']],
+            [['product_id'], 'exist', 'skipOnError' => true, 'targetClass' => Product::class, 'targetAttribute' => ['product_id' => 'id']],
+        ];
+    }
+
+    
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrder()
+    {
+        return $this->hasOne(Order::class, ['id' => 'order_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProduct()
+    {
+        return $this->hasOne(Product::class, ['id' => 'product_id']);
+    }
+}
